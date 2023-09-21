8f9ad1621fda961cd4560052aba229a9993cbea0
Neil Wang <neil@scrumaster.org>
2023-09-21 21:17:28 +0800

    first commit

---
+++ spec/try/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Try::Ruby do
+  it 'has a version number' do
+    expect(Try::Ruby::VERSION).to eq('0.1.0')
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(false)
+  end
+end
